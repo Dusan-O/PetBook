11abfa3ce83cdb171ec9d56bcef2802dbb59d500
Dusan-O <dusan.orescanin@gmail.com>
2022-08-04 01:28:54 +0200

    Add images for different friends

--- PetBook/ViewController.swift
+++ PetBook/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var containerView: UIView!
     
+    @IBOutlet var friendsIV: [UIImageView]!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupcontainerView()
+        setupFriends()
     }
     
     func setupcontainerView() {
@@ -22,6 +24,16 @@ class ViewController: UIViewController {
         containerView.layer.shadowRadius = 4
         containerView.layer.shadowOpacity = 0.75
         containerView.layer.shadowOffset = CGSize(width: 0, height: -4)
+    }
+    
+    func setupFriends() {
+        friendsIV.forEach { friend in
+            let friendFrame = friend.frame
+            let corner = friendFrame.height / 2
+            friend.layer.cornerRadius = corner
+            friend.layer.borderColor = UIColor.systemBackground.cgColor
+            friend.layer.borderWidth = 2
+        }
     }
 
 
