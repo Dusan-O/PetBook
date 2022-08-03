fb396ff5617772d770ac33eb30bf8b855456f6ad
Dusan-O <dusan.orescanin@gmail.com>
2022-08-04 01:03:51 +0200

    Add 4 assets pics, bckg, and UIImageViews + Radius, color, opacity & offset to containerView

--- PetBook/ViewController.swift
+++ PetBook/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupcontainerView()
+    }
+    
+    func setupcontainerView() {
+        containerView.layer.cornerRadius = 20
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowRadius = 4
+        containerView.layer.shadowOpacity = 0.75
+        containerView.layer.shadowOffset = CGSize(width: 0, height: -4)
     }
 
 
